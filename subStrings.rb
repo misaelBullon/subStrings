52a3fba4b1671bc73e18bceb1892671f8e03ad8a
misaelBullon <misa.esteban@gmail.com>
2020-11-09 22:18:23 -0300

    Casi final

--- subStrings.rb
+++ subStrings.rb
@@ -5,23 +5,33 @@ cadena = gets.chomp
 
 
 def substrings(string, diccionario)
+    myHash = Hash.new
     lastIndex = string.length - 1
     i = 0
     array = string.split
-    puts array
+
     array.each do |item|
         i = 0
         lastIndex = item.length - 1
+        j = lastIndex-1
+
         while i < item.length do
             palabra = item[i..lastIndex]
             if diccionario.include?(palabra)
-                puts "#{palabra} encontrada"
+                myHash[palabra] = +1
             end
             i += 1
         end
+        while j >= 0 do
+            palabra = item[0..j]
+            if diccionario.include?(palabra)
+                myHash[palabra] =  +1
+            end
+            j -= 1
+        end
     end
-
+    return myHash
 end
 
-substrings(cadena, dictionary)
-#p myHash
+my_hash = substrings(cadena, dictionary)
+p my_hash
